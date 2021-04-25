a93bec48b57877da2e6ccdd51126a3df1287eace
Rahul Nolakha <rahulrdhp@gmail.com>
2021-04-25 21:10:54 +0530

    Remove certificate related code from the test file
    Remove certificate related code from exchangerate repository test file
    as it also removed from exchange rate API.latesttt

--- inventory/urls.py
+++ inventory/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('detailHardware/',TemplateView.as_view(template_name='detail_hardware.html')),
     path('accounts/',include('django.contrib.auth.urls')),
     path('validatelogin/',views.ValidateLogin.as_view()),
+    path('validatelogin/',views.ValidateLogin.as_view()),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
